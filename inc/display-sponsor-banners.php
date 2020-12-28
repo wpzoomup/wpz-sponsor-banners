e0839fe3f203ef121a7800b6caf2ed82565ea17b
lunaluna <web@lunalunadesign.com>
2020-12-28 14:52:15 +0900

    ダミーの削除

--- inc/display-sponsor-banners.php
+++ inc/display-sponsor-banners.php
@@ -18,19 +18,14 @@ function display_sponsor_banners() {
 	if ( $banner_posts ) :
 		?>
 <aside class="wpz-footer-banner c-container">
-	<h2 class="smb-section__title">スポンサーさま</h2>
+	<h2 class="smb-section__title">スポンサー各社さま</h2>
 	<ul class="wpz-footer-banner__box">
 		<?php
-		// foreach ( $banner_posts as $post ) :
-		// 	$sponsor_name = $post->title;
-		// 	$logo_url     = get_the_post_thumbnail_url( get_the_ID(), 'medium' );
-		// 	$page_link    = get_post_meta( get_the_ID(), 'pagelink', true );
-
-
-			$sponsor_name = '類人猿 by mgn';
-			$logo_url     = 'http://127.0.0.1:8080/wp-content/uploads/2020/12/zoomup-ruijinen-logo.jpg';
-			$page_link    = 'https://rui-jin-en.com/';
-?>
+		foreach ( $banner_posts as $post ) :
+			$sponsor_name = $post->post_title;
+			$logo_url     = get_the_post_thumbnail_url( get_the_ID(), 'medium' );
+			$page_link    = get_post_meta( get_the_ID(), 'pagelink', true );
+			?>
 		<li>
 			<a href="<?php echo esc_url( $page_link ); ?>" target="_blank" rel="noreferrer noopener">
 				<figure>
@@ -39,62 +34,10 @@ function display_sponsor_banners() {
 				<h3><?php echo esc_html( $sponsor_name ); ?></h3>
 			</a>
 		</li>
-
-
-
-		<li>
-			<a href="<?php echo esc_url( $page_link ); ?>" target="_blank" rel="noreferrer noopener">
-				<figure>
-					<img src="<?php echo esc_url( $logo_url ); ?>" alt="<?php echo esc_html( $sponsor_name . 'さまロゴマーク' ); ?>">
-				</figure>
-				<h3><?php echo esc_html( $sponsor_name ); ?></h3>
-			</a>
-		</li>
-		<li>
-			<a href="<?php echo esc_url( $page_link ); ?>" target="_blank" rel="noreferrer noopener">
-				<figure>
-					<img src="<?php echo esc_url( $logo_url ); ?>" alt="<?php echo esc_html( $sponsor_name . 'さまロゴマーク' ); ?>">
-				</figure>
-				<h3><?php echo esc_html( $sponsor_name ); ?></h3>
-			</a>
-		</li>
-		<li>
-			<a href="<?php echo esc_url( $page_link ); ?>" target="_blank" rel="noreferrer noopener">
-				<figure>
-					<img src="<?php echo esc_url( $logo_url ); ?>" alt="<?php echo esc_html( $sponsor_name . 'さまロゴマーク' ); ?>">
-				</figure>
-				<h3><?php echo esc_html( $sponsor_name ); ?></h3>
-			</a>
-		</li>
-		<li>
-			<a href="<?php echo esc_url( $page_link ); ?>" target="_blank" rel="noreferrer noopener">
-				<figure>
-					<img src="<?php echo esc_url( $logo_url ); ?>" alt="<?php echo esc_html( $sponsor_name . 'さまロゴマーク' ); ?>">
-				</figure>
-				<h3><?php echo esc_html( $sponsor_name ); ?></h3>
-			</a>
-		</li>
-		<li>
-			<a href="<?php echo esc_url( $page_link ); ?>" target="_blank" rel="noreferrer noopener">
-				<figure>
-					<img src="<?php echo esc_url( $logo_url ); ?>" alt="<?php echo esc_html( $sponsor_name . 'さまロゴマーク' ); ?>">
-				</figure>
-				<h3><?php echo esc_html( $sponsor_name ); ?></h3>
-			</a>
-		</li>
-		<li>
-			<a href="<?php echo esc_url( $page_link ); ?>" target="_blank" rel="noreferrer noopener">
-				<figure>
-					<img src="<?php echo esc_url( $logo_url ); ?>" alt="<?php echo esc_html( $sponsor_name . 'さまロゴマーク' ); ?>">
-				</figure>
-				<h3><?php echo esc_html( $sponsor_name ); ?></h3>
-			</a>
-		</li>
-
-<?php
-		// endforeach;
+			<?php
+		endforeach;
 		?>
-	</ul>
+	<ul>
 </aside>
 		<?php
 	endif;
